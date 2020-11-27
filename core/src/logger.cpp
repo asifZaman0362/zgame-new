7be173c0f327d9ebac0d83ac88f722a8e3dce70f
asifZaman0362 <zero@362bits.com>
2020-11-27 18:42:40 +0530

    move definitions to header

--- core/src/logger.cpp
+++ core/src/logger.cpp
@@ -12,24 +12,7 @@
 namespace tst::zg {
     namespace Logger {
 
-        #define BLACK       "\x1B[30m"
-        #define RED         "\x1B[31m"
-        #define GREEN       "\x1B[32m"
-        #define YELLOW      "\x1B[33m"
-        #define BLUE        "\x1B[34m"
-        #define MAGENTA     "\x1B[35m"
-        #define WHITE       "\x1B[37m"
-        #define BLACK_B     "\x1B[90m"
-        #define RED_B       "\x1B[91m"
-        #define GREEN_B     "\x1B[92m"
-        #define YELLOW_B    "\x1B[93m"
-        #define BLUE_B      "\x1B[94m"
-        #define MAGENTA_B   "\x1B[95m"
-        #define WHITE_B     "\x1B[97m"
-        #define RESET       "\033[0m"
-
         #define SET_COLOR(A) std::cout << A
-
 
 
         static std::vector<std::string> logs;
